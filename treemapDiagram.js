8eee10b6f9917f3c44d0a1685b7f49dfe9576fb8
Jonathan Howard <2773262@gmail.com>
2021-02-26 11:14:13 -0500

    Add treemap tiles

--- treemapDiagram.js
+++ treemapDiagram.js
@@ -2,19 +2,19 @@ treemapDiagram(window.d3).catch(console.error);
 
 async function treemapDiagram(d3) {
   const movieData = await d3.json(
-    "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json"
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json",
   );
   console.log(movieData);
 
   const margin = { top: 100, right: 20, bottom: 20, left: 20 };
-  const width = 954 + margin.right + margin.left;
-  const height = 954 + margin.top + margin.bottom;
+  const width = 954;
+  const height = 954;
 
   const root = d3.select("#root");
 
   const svg = root.append("svg")
-    .attr("width", width)
-    .attr("height", height)
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
     .attr("class", "centered");
 
   svg.selectAll("text")
@@ -35,4 +35,26 @@ async function treemapDiagram(d3) {
     .attr("x", width * 0.5)
     .attr("y", d => d.y)
     .text(d => d.text);
+
+  const treemap = d3.treemap()
+    .size([width, height]);
+
+  const rootNode = treemap(
+    d3.hierarchy(movieData)
+      .sum(d => d.value)
+      .sort((a, b) => (b.value - a.value)),
+  );
+
+  const chart = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const tiles = chart.selectAll("g")
+    .data(rootNode.leaves())
+    .join("g")
+    .attr("transform", d => `translate(${d.x0}, ${d.y0})`);
+
+  tiles.append("rect")
+    .attr("class", "tile")
+    .attr("width", d => (d.x1 - d.x0))
+    .attr("height", d => (d.y1 - d.y0));
 }
